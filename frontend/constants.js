6d15ce1b09d1302066a87826d52bafc87f061fb5
Yosepha <47732841+Yosepha@users.noreply.github.com>
2025-04-21 13:01:41 -0500

    Delete frontend directory

--- frontend/constants.js
+++
@@ -1,4 +0,0 @@
-export const BASE_API_URL = "https://nearby-lionfish-more.ngrok-free.app";
-
-//"https://nearby-lionfish-more.ngrok-free.app"; (Y)
-//"https://noticeably-fine-gopher.ngrok-free.app";(H)
